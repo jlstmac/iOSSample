f961e4fca67bf2020c06ad15c6a640e14b8bca59
jianglinshan@qiyi.com <jlstmac@gmail.com>
2016-02-03 16:36:22 +0800

    first commit

---
+++ UISample/UISample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISample
+//
+//  Created by jianglinshan@qiyi.com on 16/2/3.
+//  Copyright © 2016年 jlstmac@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISample/UISample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISample
+//
+//  Created by jianglinshan@qiyi.com on 16/2/3.
+//  Copyright © 2016年 jlstmac@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
