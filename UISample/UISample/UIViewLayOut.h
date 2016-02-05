ca93a12d194ea1758d0fc52038fd41c451d43e35
jianglinshan@qiyi.com <jlstmac@gmail.com>
2016-02-05 15:55:40 +0800

    commit test

---
+++ UISample/UISample/UIViewLayOut.h
@@ -0,0 +1,13 @@
+//
+//  UIViewLayOut.h
+//  UISample
+//
+//  Created by jianglinshan@qiyi.com on 16/2/4.
+//  Copyright © 2016年 jlstmac@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewLayOut : UIView
+
+@end
---
+++ UISample/UISample/UIViewLayOut.m
@@ -0,0 +1,13 @@
+//
+//  UIViewLayOut.m
+//  UISample
+//
+//  Created by jianglinshan@qiyi.com on 16/2/4.
+//  Copyright © 2016年 jlstmac@gmail.com. All rights reserved.
+//
+
+#import "UIViewLayOut.h"
+
+@implementation UIViewLayOut
+
+@end
